4f4770cafe598a58e405e1a33b8a81fd28f6c55c
vincent0616 <coji204396@gmail.com>
2023-11-08 12:38:07 +0900

    aa

---
+++ src/main/java/com/education/lledu/LleduApplication.java
@@ -0,0 +1,13 @@
+package com.education.lledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LleduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LleduApplication.class, args);
+	}
+
+}
